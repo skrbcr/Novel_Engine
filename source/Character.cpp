8e9f84f99ee03c382790bac5f9dcc1134b3d0ebd
skrbcr <90087433+skrbcr@users.noreply.github.com>
2021-12-10 13:31:03 +0900

    Character class を作成中、基本的な機能を追加、今後テストする

--- source/Character.cpp
+++ source/Character.cpp
@@ -1,6 +1,61 @@
 #include "Character.h"
 
 namespace Game {
-	vector<vector<int>> vpos = vector<vector<int>>(0, vector<int>(2));
+    void Character::SetCharaPos(size_t index, string_view strPos, int x, int y) {
+        if (index < 0 || index >= vchara.size()) {
+            return;
+        }
+        vchara[index].gh_body = 0;
+        vchara[index].gh_face = 0;
+        vchara[index].strPos = strPos;
+        vchara[index].x = x;
+        vchara[index].y = y;
+    }
 
+    //void Character::MakeCharaImg() {
+    //    vchara = vector<CharaImg>(nSizeVchara);
+    //}
+
+    void Character::SetBody(int gh, string_view strPos) {
+        for (auto& pos : vchara) {
+            if (strPos == pos.strPos) {
+                pos.gh_body = gh;
+            }
+        }
+    }
+
+    void Character::SetFace(int gh, string_view strPos) {
+        for (auto& pos : vchara) {
+            if (strPos == pos.strPos) {
+                pos.gh_face = gh;
+            }
+        }
+    }
+
+    void Character::DeleteBody(string_view strPos) {
+        for (auto& pos : vchara) {
+            if (strPos == pos.strPos) {
+                pos.gh_body = 0;
+            }
+        }
+    }
+
+    void Character::DeleteFace(string_view strPos) {
+        for (auto& pos : vchara) {
+            if (strPos == pos.strPos) {
+                pos.gh_face = 0;
+            }
+        }
+    }
+
+    void Character::Main() {
+        for (const auto& pos : vchara) {
+            if (pos.gh_body != 0) {
+                DrawGraph(pos.x, pos.y, pos.gh_body, TRUE);
+            }
+            if (pos.gh_face != 0) {
+                DrawGraph(pos.x, pos.y, pos.gh_face, TRUE);
+            }
+        }
+    }
 }
--- source/Character.h
+++ source/Character.h
@@ -6,24 +6,56 @@
 using std::vector;
 using std::string_view;
 
+struct CharaImg
+{
+	int gh_body = 0;		// body グラフィックハンドル
+	int gh_face = 0;		// face グラフィックハンドル
+	string strPos = "";		// 配置コマンド
+	int x = 0;				// 画像x座標
+	int y = 0;				// 画像y座標
+};
+
+//struct CharaPos
+//{
+//	string strPos = "";
+//	int x = 0;
+//	int y = 0;
+//};
+
 namespace Game {
 	class Character
 	{
 	private:
-		int gh_body = 0;		// body グラフィックハンドル
-		int gh_face = 0;		// face グラフィックハンドル
-
-	public:
-		static vector<vector<int>> vpos;		// 画像配置座標
+		//vector<CharaPos> vpos = vector<CharaPos>(0);		// 画像配置情報配列
+		vector<CharaImg> vchara = vector<CharaImg>(0);		// キャラクター画像データ配列
+		size_t nSizeVchara = 0;
 
 	public:
 		Character() {
 
 		}
 
+		Character(size_t nSizeVchara) {
+			vchara = vector<CharaImg>(nSizeVchara);
+		}
+
 		~Character() {
 
 		}
+
+		/// <summary>
+		/// 画像情報配列に追加
+		/// </summary>
+		/// <param name="index">要素番号</param>
+		/// <param name="strPos">位置コマンド</param>
+		/// <param name="x">画像左上 x座標</param>
+		/// <param name="y">画像左上 座標</param>
+		void SetCharaPos(size_t index, string_view strPos, int x, int y);
+
+		/// <summary>
+		/// CharaImg 配列を生成
+		/// </summary>
+		//void MakeCharaImg();
 
 		/// <summary>
 		/// body 画像を設定
@@ -32,6 +64,29 @@ namespace Game {
 		/// <param name="strPos"></param>
 		void SetBody(int gh, string_view strPos);
 
+		/// <summary>
+		/// face 画像を設定
+		/// </summary>
+		/// <param name="gh"></param>
+		/// <param name="strPos"></param>
+		void SetFace(int gh, string_view strPos);
+
+		/// <summary>
+		/// body 画像を削除
+		/// </summary>
+		/// <param name="strPos">配置コマンド</param>
+		void DeleteBody(string_view strPos);
+
+		/// <summary>
+		/// face 画像を削除
+		/// </summary>
+		/// <param name="strPos">配置コマンド</param>
+		void DeleteFace(string_view strPos);
+
+		/// <summary>
+		/// メイン関数
+		/// </summary>
+		void Main();
 	};
 }
 
