da3a618ec4e779e95696402d516d72f15ab4b995
skrbcr <90087433+skrbcr@users.noreply.github.com>
2022-08-01 18:03:29 +0900

    GameWindow class の追加。 - GameWindow: Main より移植 - Main.cpp: GameWindow へ移植 - Global.h: stdexcept のインクルードを追加

---
+++ source/GameWindow.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "nlohmann/json.hpp"
+#include "Global.h"
+#include "Title.h"
+#include "Menu.h"
+#include "Place.h"
+#include "Choice.h"
+#include "Button.h"
+#include "BGM.h"
+
+using std::runtime_error;
+
+namespace Game {
+	class GameWindow {
+	private:
+		json js_cfg = json();		// Config.json
+		Title title = Title();		// タイトル画面
+		Menu menu = Menu();			// メニュー
+		Place place = Place();		// 場所インスタンス
+
+		char key[256] = { 0 };		// キー配列格納
+		int status = 0;				// ステータス番号
+		int counter = 0;			// フレームカウンタ
+		int snum = 0;				// シーン管理番号
+		int resEvent = 0;			// イベント返り値
+		int resEvDet = 0;			// イベント返り値詳細
+		bool playFinFlag = false;	// ゲーム本編終了フラグ
+
+	public:
+		GameWindow();
+		~GameWindow();
+		void Main();
+
+	private:
+		void GameInit();			// ゲームの初期化。本編移行前に行う
+		void MakeHandles();			// 各種ハンドルの作成
+		void DeleteHandles();		// 各種ハンドルの削除
+		void LoadConfig();			// Config.jsonの読み込み・ウィンドウの設定
+		void SetConfig();			// Config.json の適用
+		void GameMain();			// ゲームのメインループ処理
+		int GamePlay();				// ゲーム本編
+	};
+}
